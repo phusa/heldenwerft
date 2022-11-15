bc45affcd65bbdfb079421a3db398d25f8fe67ac
phusa <ph@usa.com>
2022-11-15 09:17:40 -0600

    removed sqlite from strapi package.json

--- app/.svelte-kit/types/src/routes/proxy+page.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-/** @param {Parameters<import('./$types').PageLoad>[0]} event */
-export async function load({ data }) {
-    const res = await fetch('http://localhost:1337/api/projects');
-    const data = await res.json();
-   
-    return { data };
-  }
