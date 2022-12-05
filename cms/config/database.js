b48a66182459f44c51f8177ae56b1680364af159
phusa <ph@usa.com>
2022-12-05 13:58:05 -0600

    gitignore changes

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,15 +1,5 @@
 const path = require('path');
 
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
