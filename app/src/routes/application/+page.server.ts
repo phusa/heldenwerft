ff5aeb6b96f84a6f0f9566c9e5ac18a8c30bf3dc
phusa <ph@usa.com>
2023-01-25 10:57:45 -0600

    form submit test

--- app/src/routes/application/+page.server.ts
+++ app/src/routes/application/+page.server.ts
@@ -1,8 +1,20 @@
 import type { Actions } from './$types';
+import { Client } from '$lib/client';
  
 export const actions = {
   default: async (event) => {
     // TODO log the user in
-    console.log('sdcsdc');
+    console.log(event.request.formData());
+
+    let gql = `mutation {
+      createApplication (data: { Reason: "ssss"}) {
+        data {
+          attributes {
+            Reason
+          }
+        }
+      }
+    }`;
+    //Client(gql);
   }
 } satisfies Actions;
--- app/src/routes/application/+page.svelte
+++ app/src/routes/application/+page.svelte
@@ -3,13 +3,13 @@
 <h1>TODO: Application form</h1>
 
 <form method="POST">
-    <label>
-      Email
-      <input name="email" type="email">
-    </label>
-    <label>
-      Password
-      <input name="password" type="password">
-    </label>
-    <button>Log in</button>
-  </form>
+  <label>
+    Email
+    <input name="email" type="email">
+  </label>
+  <label>
+    Password
+    <input name="password" type="password">
+  </label>
+  <button>Log in</button>
+</form>
