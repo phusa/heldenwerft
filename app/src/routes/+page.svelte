665a31ac78ac61cfcc6bb1e72d7e1e3e29a3dd0a
phusa <ph@usa.com>
2023-01-25 17:48:38 -0600

    css wave

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,75 +1,95 @@
-<div class="hero">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://placeimg.com/600/400/people" class="" alt=""/>
-        <div class="">
-            <h1 class="text-4xl font-bold">Service Learning Projekt</h1>
-            <h2 class="text-2xl">Lernen durch Engagement</h2>
-            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary">Mehr erfahren</button>
-        </div>
-    </div>
+<div class="h-[450px]">
+<div class="absolute w-screen left-0 bg-slate-400">
+	<div class="relative">
+		<div class="wave" />
+		<div class="hero">
+			<div class="hero-content flex-col lg:flex-row-reverse">
+				<!--<img src="https://placeimg.com/600/400/people" class="" alt=""/>-->
+				<div class="">
+					<h1 class="text-4xl font-bold">Service Learning Projekt</h1>
+					<h2 class="text-2xl">Lernen durch Engagement</h2>
+					<p class="py-6">
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+						exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+					</p>
+					<button class="btn btn-primary">Mehr erfahren</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex bg-base-400 container ml-auto mr-auto bg-slate-200 px-5 mt-10">
+		<div class="card lg:card-side">
+			<figure><img src="https://placeimg.com/200/200/people" alt="" class="rounded-full" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">Lernen durch Engagement</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae reiciendis quam at
+					error accusantium labore enim repellendus
+				</p>
+			</div>
+		</div>
+
+		<div class="card lg:card-side">
+			<figure><img src="https://placeimg.com/200/200/nature" alt="" class="rounded-full" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">Lenen durch Engagement</h2>
+				<p>
+					Culpa doloribus eveniet optio recusandae minus numquam iste tempore nulla omnis saepe
+					veritatis.
+				</p>
+			</div>
+		</div>
+
+		<div class="card lg:card-side">
+			<figure><img src="https://placeimg.com/200/200/tech" alt="" class="rounded-full" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">Lenen durch Engagement</h2>
+				<p>
+					Molestiae reiciendis quam at error accusantium labore enim repellendus, culpa doloribus
+					eveniet optio recusandae minus numquam iste tempore nulla omnis saepe veritatis.
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
-
-<div class="flex bg-base-400">
-    <div class="card lg:card-side">
-        <figure><img src="https://placeimg.com/200/200/people" alt="" class="rounded-full"/></figure>
-        <div class="card-body">
-            <h2 class="card-title">Lernen durch Engagement</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae reiciendis quam at error accusantium labore enim repellendus</p>
-        </div>
-    </div>
-
-    <div class="card lg:card-side">
-        <figure><img src="https://placeimg.com/200/200/nature" alt="" class="rounded-full"/></figure>
-        <div class="card-body">
-            <h2 class="card-title">Lenen durch Engagement</h2>
-            <p>Culpa doloribus eveniet optio recusandae minus numquam iste tempore nulla omnis saepe veritatis.</p>
-        </div>
-    </div>
-
-    <div class="card lg:card-side">
-        <figure><img src="https://placeimg.com/200/200/tech" alt="" class="rounded-full"/></figure>
-        <div class="card-body">
-            <h2 class="card-title">Lenen durch Engagement</h2>
-            <p>Molestiae reiciendis quam at error accusantium labore enim repellendus, culpa doloribus eveniet optio recusandae minus numquam iste tempore nulla omnis saepe veritatis.</p>
-        </div>
-    </div>
 </div>
-
 <div class="card lg:card-side">
-    <figure><img src="https://placeimg.com/600/600/tech" alt=""/></figure>
-    <div class="card-body">
-        <h1 class="card-title">Lenen durch Engagement</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
-        <p>Molestiae reiciendis quam at error accusantium labore enim repellendus, culpa doloribus eveniet optio recusandae minus numquam iste tempore nulla omnis saepe veritatis.</p>
-        <div class="card-actions">
-            <button class="btn btn-primary">Projekt vorschlagen</button>
-          </div>
-    </div>
+	<figure><img src="https://placeimg.com/600/600/tech" alt="" /></figure>
+	<div class="card-body">
+		<h1 class="card-title">Lenen durch Engagement</h1>
+		<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+		<p>
+			Molestiae reiciendis quam at error accusantium labore enim repellendus, culpa doloribus
+			eveniet optio recusandae minus numquam iste tempore nulla omnis saepe veritatis.
+		</p>
+		<div class="card-actions">
+			<button class="btn btn-primary">Projekt vorschlagen</button>
+		</div>
+	</div>
 </div>
 
-<div class="card lg:card-side">
-    <div class="card-body">
-        <h1 class="card-title ">Lernen durch Engagement</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae reiciendis quam at error accusantium labore enim repellendus</p>
-        <div class="card-actions">
-            <button class="btn btn-primary">Studiengang</button>
-        </div>
-    </div>
-    <figure><img src="https://placeimg.com/600/600/people" alt=""/></figure>
+<div class="card lg:card-side mt-10">
+	<div class="card-body">
+		<h1 class="card-title ">Lernen durch Engagement</h1>
+		<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae reiciendis quam at error
+			accusantium labore enim repellendus
+		</p>
+		<div class="card-actions">
+			<button class="btn btn-primary">Studiengang</button>
+		</div>
+	</div>
+	<figure><img src="https://placeimg.com/600/600/people" alt="" /></figure>
 </div>
-<div class="divider"></div>
+<div class="divider" />
 <div class="flex">
-
-<ul class="steps">
-    <li class="step step-primary">Wir sind</li>
-    <li class="step step-primary">Wir brauchen</li>
-    <li class="step">Kontakt</li>
-    <li class="step">Abschicken</li>
-  </ul>
-
+	<ul class="steps">
+		<li class="step step-primary">Wir sind</li>
+		<li class="step step-primary">Wir brauchen</li>
+		<li class="step">Kontakt</li>
+		<li class="step">Abschicken</li>
+	</ul>
 </div>
-<div class="divider"></div>
-
-
+<div class="divider" />
