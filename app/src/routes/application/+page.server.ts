ec683559c60e94fb33185cef53be01d0869e8509
phusa <ph@usa.com>
2023-01-25 09:03:47 -0600

    updated dev dependencies to allow for typescript 4.9 as needed by sveltekit form actions

---
+++ app/src/routes/application/+page.server.ts
@@ -0,0 +1,8 @@
+import type { Actions } from './$types';
+ 
+export const actions = {
+  default: async (event) => {
+    // TODO log the user in
+    console.log('sdcsdc');
+  }
+} satisfies Actions;
--- app/src/routes/application/+page.svelte
+++ app/src/routes/application/+page.svelte
@@ -1,1 +1,15 @@
+
+
 <h1>TODO: Application form</h1>
+
+<form method="POST">
+    <label>
+      Email
+      <input name="email" type="email">
+    </label>
+    <label>
+      Password
+      <input name="password" type="password">
+    </label>
+    <button>Log in</button>
+  </form>
